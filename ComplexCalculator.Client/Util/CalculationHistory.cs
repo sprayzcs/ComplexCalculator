411561e2b73fc57714c00f6646ae4324061ef565
sprayzcs <lennart@kampshoff.xyz>
2019-03-13 10:26:40 +0100

    Beschriebene Komplexe Zahl

---
+++ ComplexCalculator.Client/Util/CalculationHistory.cs
@@ -0,0 +1,23 @@
+﻿using ComplexCalculator.Number;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexCalculator.Client.Util
+{
+    class CalculationHistory
+    {
+        internal ComplexNumber ComplexNumber1 { get; }
+        internal ComplexNumber ComplexNumber2{ get; }
+
+        internal CalculationHistory(ComplexNumber complexNumber1, ComplexNumber complexNumber2)
+        {
+            ComplexNumber1 = complexNumber1;
+            ComplexNumber2 = complexNumber2;
+        }
+
+
+    }
+}
---
+++ ComplexCalculator/Number/DescribedComplexNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexCalculator.Number
+{
+    public class DescribedComplexNumber : ComplexNumber
+    {
+        private static int _id = 1;
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public DescribedComplexNumber(double? real = null, double? imaginary = null, double? angle = null, double? amount = null) : base(real, imaginary, angle, amount)
+        {
+            this.Id = _id;
+            _id++;
+        }
+
+        public DescribedComplexNumber(string description, double? real = null, double? imaginary = null, double? angle = null, double? amount = null) : this(real, imaginary, angle, amount)
+        {
+            this.Description = description;
+        }
+    }
+}
